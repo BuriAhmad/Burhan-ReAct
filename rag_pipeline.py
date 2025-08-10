bc1fedbcd127d1a37b8fc7b779c95481dffb9c25
BuriAhmad <b.ahmadkhan3@gmail.com>
2025-08-10 13:09:24 +0500

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -64,14 +64,15 @@ class RAGPipeline:
             augmented_prompt = f"""Using the information contained in the context,
 give a comprehensive answer to the question.
 Respond only to the question asked, response should be concise and relevant to the question.
-Provide the number of the source document used.
-If the answer cannot be deduced from the context, answer with "no information foud".
+Provide the number of the source document when relevant.
+If the answer cannot be deduced from the context, do not give an answer.
 
 ## RETRIEVED CONTEXT:
 {context}
 
 ## USER QUESTION:
 {user_query}
+
 """
             
             state["augmented_prompt"] = augmented_prompt
